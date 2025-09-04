f0a400a4ad956a94bc018223c0865dddbe009ca4
Shadow <slappingsloth@gmail.com>
2025-09-04 12:29:30 +0800

    add admin models

--- habit/admin.py
+++ habit/admin.py
@@ -1,2 +1,21 @@
 from django.contrib import admin
+from .models import Habit, DailyRecord
 
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'user',
+        'target',
+        'unit',
+    ]
+    list_filter = ['user']
+
+@admin.register(DailyRecord)
+class DailyRecordAdmin(admin.ModelAdmin):
+    list_display = [
+        'habit',
+        'date',
+        'amount_achieved',
+    ]
+    list_filter = ['date','habit']
