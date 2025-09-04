1242f90eee52477f65421a0e759b4d7882acc6e2
Shadow <slappingsloth@gmail.com>
2025-09-05 00:16:32 +0800

    add switch to databases

--- config/settings.py
+++ config/settings.py
@@ -63,17 +63,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
+if DEBUG:
+    
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST':'db',
-        'PORT':5432,
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST':'db',
+            'PORT':5432,
+            
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
